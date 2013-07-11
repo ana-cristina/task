5768031656b74bb5d4844eaaae9a67d90c9cc131
Ana Cristina <turleaana@gmail.com>
2013-07-11 11:15:23 +0300

    validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ActiveRecord::Base
   attr_accessible :done, :name
+  validates :name, :presence => true  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ TaskApp::Application.routes.draw do
   
   resources :tasks
   root :to => "tasks#index"
-  match "show" => "tasks#show"
 end
